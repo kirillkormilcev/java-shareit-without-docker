39610a53eaafecc80ca8936ad5b79506454e63ff
kirill <kirill@kormilcev.ru>
2022-09-18 22:21:26 +0300

    fix MethodArgumentNotValidException

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.error;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.error.exception.IncorrectRequestParamException;
@@ -37,6 +38,13 @@ public class ErrorHandler {
     public ResponseEntity<ErrorResponse> handleThrowable(final Throwable e) {
         log.warn(e.getMessage(), e);
         return new ResponseEntity<>(new ErrorResponse("Неожиданная ошибка.", e.getMessage()),
-                HttpStatus.BAD_GATEWAY);
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.warn(e.getMessage(), e);
+        return new ResponseEntity<>(new ErrorResponse("Не корректно(-ы)е поле(-я) пользователя.", e.getMessage()),
+                HttpStatus.BAD_REQUEST);
     }
 }
